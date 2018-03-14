2704059d37a8b1adbe5b2f55ab6ce507e31ae9f6
Nyle Musgrave <nyle@fivepackcreative.com>
2018-03-14 12:31:49 -0500

    Removed saving captured images to library.
    Signed-off-by: Nyle Musgrave <nyle@fivepackcreative.com>

--- ALCameraViewController/ViewController/CameraViewController.swift
+++ ALCameraViewController/ViewController/CameraViewController.swift
@@ -518,25 +518,8 @@ open class CameraViewController: UIViewController {
     internal func saveImage(image: UIImage) {
         let spinner = showSpinner()
         cameraView.preview.isHidden = true
-
-		if allowsLibraryAccess {
-        _ = SingleImageSaver()
-            .setImage(image)
-            .onSuccess { [weak self] asset in
-                self?.layoutCameraResult(asset: asset)
-                self?.hideSpinner(spinner)
-            }
-            .onFailure { [weak self] error in
-                self?.toggleButtons(enabled: true)
-                self?.showNoPermissionsView(library: true)
-                self?.cameraView.preview.isHidden = false
-                self?.hideSpinner(spinner)
-            }
-            .save()
-		} else {
-			layoutCameraResult(uiImage: image)
-			hideSpinner(spinner)
-		}
+        layoutCameraResult(uiImage: image)
+        hideSpinner(spinner)
     }
 	
     internal func close() {
